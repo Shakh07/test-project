abebf06c3fad14a458aaa39bd0b9de9d9a8e1ea3
Shohin <kesamirov.071197@mail.ru>
2022-03-16 07:29:21 +0300

    create stage component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="mt-2">
+    <Stages v-for="stage in stages" :key="stage.id" :stage="stage" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Stages from "./components/Stages.vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { Stages },
+  data() {
+    return {
+      stages: [
+        { id: 1, number: 1 },
+        { id: 2, number: 2 },
+        { id: 3, number: 3 },
+        { id: 4, number: 4 },
+        { id: 5, number: 5 },
+      ],
+    };
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ src/components/Stages.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="border h-24 flex flex-col gap-y-2">
+    <p>{{ stage.number }}</p>
+    <div
+      @click="isClicked = true"
+      class="wrapper cursor-pointer border border-blue-400 w-7 flex justify-center p-1"
+      :class="{ isClicked: isClicked }"
+    >
+      <div
+        class="border border-blue-700 w-5 flex justify-center p-1 rounded-full"
+        :class="{ isClicked: isClicked }"
+      >
+        <input :id="stage.number" type="radio" class="hidden" />
+        <label
+          :for="stage.number"
+          class="w-2 h-2 inline-block rounded-full bg-blue-700 cursor-pointer"
+          :class="{ isChecked: isClicked }"
+        >
+        </label>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    stage: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      isClicked: false,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.wrapper:active {
+  background-color: rgb(231, 173, 11);
+}
+.isChecked {
+  background-color: orange;
+}
+.isClicked {
+  border: 1px solid orange;
+}
+</style>
